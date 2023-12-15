896065ea29b9deffe9cbdf8995b989847f161b6c
Adrián Primo <adrianprimob@gmail.com>
2023-12-15 23:56:31 +0100

    Lobby en funcionamiento

--- js/cliente.js
+++ js/cliente.js
@@ -1,25 +1,55 @@
 const socket = io('ws://localhost:8080');
 
-socket.on('lobbyCreated', (lobbyCode) => {
+socket.on('lobbyCreated', ()  => {
 
-    window.location.href = `quiplash.html?code=${lobbyCode}`;
-    // document.getElementById('lobby-code').innerText = `Código de sala: ${lobbyCode}`;
+});
+
+socket.on('join', (lobbyCode) => {
+
+    const codes = document.getElementsByClassName('gameCode')
+
+    for (const code of codes) {
+        code.innerText = `${lobbyCode}`;
+    }
 });
 
 socket.on('updatePlayers', (players) => {
-    const lobby = lobbies.find((l) => l.game === "Quiplash");
-    if (lobby){
-        players.forEach((player) => {
-            // const padre = document.getElementsByClassName("palo")[player.index];
-            // const icono = padre.children[0];
-            // icono.style.opacity = "1";
-            // icono.children[0].innerText = player.name;
-            // icono.style.animationPlayState = "running";
-        });
+    console.log(players.length)
+    for (const [index, player] of players.entries()) {
+        console.log(index)
+        const padre = document.getElementsByClassName("palo")[index];
+        const icono = padre.children[0];
+        icono.style.opacity = "1";
+        icono.children[0].innerText = player.name;
+        icono.style.animationPlayState = "running";
     }
+});
 
+socket.on('disconnectPlayer', (playerId, players) => {
+    const padre = document.getElementsByClassName("palo")[playerId];
+    const icono = padre.children[0];
+
+    // Ocultar el icono con una breve demora
+    icono.style.opacity = "0";
+
+    // Asegurémonos de que updatePlayers se ejecute después de ocultar el icono
+    setTimeout(() => {
+        socket.emit('updatePlayers', players);
+    }, 100); // Ajusta el tiempo según sea necesario
 });
 
 socket.on('joinError', (message) => {
     alert(message);
+    // window.location.href = index.html;
 });
+
+//TODO Obtener datos del formulario y meterlos
+
+// if crear lobby
+if (0){
+    socket.emit('create', 'Adrian', 'quiplash')
+} else {
+    socket.emit('join', 'Calvo', '0DCR');
+}
+
+
--- js/server.js
+++ js/server.js
@@ -21,45 +21,51 @@ const lobbies = [];
 http.listen(8080, () => console.log("LISTENING ON 8080"));
 
 io.on('connection', (socket) => {
-    console.log('Usuario conectado:', socket.id);
+    // console.log('Usuario conectado:', socket.id);
 
     // Unirse a una sala
     socket.on('join', (nombreJug, lobbyCode) => {
 
-        db.query('SELECT * FROM Sala WHERE codigo_sala = ?', [lobbyCode], (error, results) => {
-            if (error) {
-                console.error(error);
-                return;
-            }
-
-            const lobby = results[0];
-
-            // Realiza acciones necesarias con la información de la sala y los jugadores
-            io.to(lobbyCode).emit('updatePlayers', lobby.players);
-        });
-
-        // const lobby = lobbies.find((l) => l.code === lobbyCode);
-        // console.log(lobbyCode)
-        // console.log(lobbies)
-        // console.log("LOBBY?: " + lobby)
-        // if (lobby && lobby.players.length < 8) {
-        //     socket.join(lobbyCode);
-        //     lobby.players.push({ id: socket.id, name: nombreJug });
+        // db.query('SELECT * FROM Sala WHERE codigo_sala = ?', [lobbyCode], (error, results) => {
+        //     if (error) {
+        //         console.error(error);
+        //         return;
+        //     }
         //
+        //     const lobby = results[0];
+        //
+        //     // Realiza acciones necesarias con la información de la sala y los jugadores
         //     io.to(lobbyCode).emit('updatePlayers', lobby.players);
-        // } else {
-        //     socket.emit('joinError', 'No se puede unir a la sala');
-        // }
+        // });
+
+        const lobby = lobbies.find((l) => l.code === lobbyCode);
+
+        if (lobby && lobby.players.length < 8) {
+            socket.join(lobbyCode);
+            socket.emit('join', lobbyCode);
+            lobby.players.push({ id: socket.id, name: nombreJug});
+            io.to(lobbyCode).emit('updatePlayers', lobby.players);
+        } else {
+            socket.emit('joinError', 'No se puede unir a la sala');
+        }
     });
 
     // Crear una nueva sala
     socket.on('create', (nombreHost, juego) => {
+
+        // Creamos un nuevo lobby y lo agregamos a la lista de lobbies del servidor
         const newLobby = { code: generateRandomCode(), players: [], game: juego };
         lobbies.push(newLobby);
-        console.log("Código Lobby: " + newLobby.code)
+
+        // El socket (cliente) se conecta
         socket.join(newLobby.code);
+
+        // Metemos un nuevo jugador al lobby con el id del socket y el nombre del creador de la partida
         newLobby.players.push({ id: socket.id, name: nombreHost});
-        socket.emit('lobbyCreated', newLobby.code);
+
+        // Emitimos el evento de lobby creado a los jugadores (solo 1 en teoria)
+        socket.emit('lobbyCreated');
+        socket.emit('join', newLobby.code);
         io.to(newLobby.code).emit('updatePlayers', newLobby.players);
     });
 
@@ -68,16 +74,24 @@ io.on('connection', (socket) => {
         const lobby = lobbies.find((l) => l.players.some((p) => p.id === socket.id));
 
         if (lobby) {
+
+            if (lobby.players[0].id === socket.id){
+                io.to(lobby.code).emit('joinError', 'El host se ha desconectado de la sala');
+            }
+
+            let disconnectedPlayer = lobby.players.find(p => p.id === socket.id);
+            let disconPlayerId = lobby.players.indexOf(disconnectedPlayer)
+
             lobby.players = lobby.players.filter((p) => p.id !== socket.id);
-            io.to(lobby.code).emit('updatePlayers', lobby.players);
+
+            io.to(lobby.code).emit('disconnectPlayer', disconPlayerId, lobby.players);
         }
 
-        console.log('Usuario desconectado:', socket.id);
+
+        // console.log('Usuario desconectado:', socket.id);
     });
 });
 
 function generateRandomCode() {
-    // Implementa la lógica para generar códigos únicos
-    // Puedes usar algoritmos más avanzados dependiendo de tus necesidades
     return Math.random().toString(36).substring(2, 6).toUpperCase();
 }
