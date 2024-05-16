5815085e52b348d957d2e92d72f3ac698822d9ce
Adrián Primo <adrianprimob@gmail.com>
2024-05-16 17:26:21 +0200

    Actualizado campos de la BBDD en API

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -25,71 +25,100 @@ router.get('/prompts/:gameId/:language', (req, res) => {
     });
 });
 
-// Obtener una respuesta de seguridad para un prompt específico
-router.get('/safety-answer/:promptId/:language', (req, res) => {
+// Obtener una respuesta de emergencia para un prompt específico
+router.get('/safety-answers/:promptId/:language', (req, res) => {
     const {promptId, language} = req.params;
-    const query = 'SELECT id_answer, id_prompt, text FROM safety_answers WHERE id_prompt = ? AND idioma = ?';
+    const query = 'SELECT id_answer, id_prompt, text FROM respuestas_emergencia WHERE id_prompt = ? AND idioma = ?';
     db.query(query, [promptId, language], (error, answers) => {
         if (error) {
             console.error(error);
             res.status(500).json({error: 'Error al obtener la respuesta de seguridad'});
             return;
         }
-        res.json(answers);
+        const randomIndex = Math.floor(Math.random() * answers.length);
+        const randomAnswer = answers[randomIndex];
+        res.json(randomAnswer);
     });
 });
 
-router.post('/answer', (req, res) => {
+router.post('/send-answer', (req, res) => {
     const {promptId, answer, playerId, round, lobbyCode} = req.body;
 
-    // TODO Hacer consulta a BBDD para obtener ID sala usando lobbyCode
-
-    db.query('INSERT INTO answers (prompt_id, text, player_id, round) VALUES (?, ?)', [promptId, answer, playerId, round], (error) => {
-        if (error) {
-            console.error(error);
-            res.status(500).json({error: 'Error al registrar la respuesta de seguridad'});
-            return;
-        }
-        res.sendStatus(200);
-    });
-});
-
-router.post('/safety-answer', (req, res) => {
-    const {promptId, playerId, round, lobbyCode} = req.body;
 
     db.query('SELECT id_sala FROM salas WHERE codigo_sala = ?', [lobbyCode], (error, result) => {
-
-
         if (error) {
             console.error(error);
             res.status(500).json({error: 'Error al obtener el id de la sala'});
             return;
         }
 
-        db.query('SELECT * FROM safety_answers WHERE prompt_id = ?', [promptId], (error, answers) => {
+        if (result.length === 0) {
+            // No se encontró ninguna sala con el código dado
+            res.status(404).json({ error: 'No se encontró ninguna sala con el código proporcionado' });
+            return;
+        }
+
+        const idSala = result[0].id_sala;
+        console.log('ENVIAR RESPUESTA idSAla: ', idSala);
+
+        db.query('INSERT INTO respuestas (id_prompt, texto, id_jugador, ronda, id_sala) VALUES (?, ?, ?, ?, ?)', [promptId, answer, playerId, round, idSala], (error) => {
             if (error) {
                 console.error(error);
-                res.status(500).json({error: 'Error al obtener las respuestas de emergencia'});
+                res.status(500).json({error: 'Error al registrar la respuesta de seguridad'});
                 return;
             }
-            // TODO Hacer consulta a BBDD para obtener ID sala usando lobbyCode
-
-
-            const answer = answers[Math.floor(Math.random() * answers.length)];
-
-            db.query('INSERT INTO answers (prompt_id, text, player_id, round) VALUES (?, ?)', [promptId, answer, playerId, round], (error) => {
-                if (error) {
-                    console.error(error);
-                    res.status(500).json({error: 'Error al registrar la respuesta de emergencia'});
-                    return;
-                }
-                res.sendStatus(200);
-            });
+            res.sendStatus(200);
         });
     });
-
-
 });
 
+// Este bloque servirá para poder crear respuestas por defecto personalizadas
+router.post('/safety-answers/create', (req, res) => {
+    const {answerId, promptId, text, language} = req.body;
+
+
+    db.query('SELECT * FROM prompts WHERE id_prompt = ?', [promptId], (error) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error al encontrar el prompt'});
+            return;
+        }
+
+
+        db.query('INSERT INTO respuestas_emergencia (id_answer,id_prompt, text, idioma) VALUES (?, ?, ?, ?)', [answerId,promptId, text, language], (error) => {
+            if (error) {
+                console.error(error);
+                res.status(500).json({error: 'Error al registrar la respuesta de emergencia'});
+                return;
+            }
+            res.sendStatus(200);
+        });
+    });
+});
+
+// Aquí podemos crear nuevos prompts para un juego
+router.post('/prompts/create', (req, res) => {
+    const {promptId, gameId, text, language} = req.body;
+    db.query('INSERT INTO prompts (id_prompt, id_juego, text, idioma) VALUES (?, ?, ?, ?)', [promptId, gameId, text, language], (error) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error al registrar el prompt'});
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
+
+router.post('/lobby/create', (req, res) => {
+    const {lobbyId, lobbyCode, gameId} = req.body;
+    db.query('INSERT INTO salas (id_sala, id_modoJuego, codigo_sala) VALUES (?)', [lobbyId, gameId, lobbyCode], (error) => {
+        if (error) {
+            console.error(error);
+            res.status(500).json({error: 'Error al registrar la sala'});
+            return;
+        }
+        res.sendStatus(200);
+    });
+});
 
 module.exports = router;
