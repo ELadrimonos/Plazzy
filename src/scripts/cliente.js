c7ed3660688b7e26d9b291ab9990c7396b178c6c
Adrián Primo <adrianprimob@gmail.com>
2024-06-18 17:14:42 +0200

    Modificar conexion cliente http

--- src/scripts/cliente.js
+++ src/scripts/cliente.js
@@ -1,8 +1,10 @@
 import io from 'socket.io-client';
 
-export const socket = io('ws://plazzy.es:8080', {
-    transports: ['websocket']
-});
+export const socket = io("http://plazzy.es:8080", {
+        cors: {
+        origin: "http://plazzy.es:8080",
+        credentials: true
+      },transports : ['websocket'] });
 
 
 socket.on('joinError', (message) => {
