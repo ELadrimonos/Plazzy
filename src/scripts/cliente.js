0a3717e8a31af652f08a2f9bb07ef0c79bd84b25
Adrián Primo <adrianprimob@gmail.com>
2024-06-23 19:28:58 +0200

    Más logs en el lado cliente.js

--- src/scripts/cliente.js
+++ src/scripts/cliente.js
@@ -1,7 +1,28 @@
 import io from 'socket.io-client';
 
-export const socket = io(`ws://plazzy.es:${process.env.PORT}`);
+export const socket = io(`ws://plazzy.es:${process.env.PORT}`,
+    {
+        transports: ['websocket'], // Ensure only WebSocket transport is used
+        reconnectionAttempts: 5, // Number of reconnection attempts
+        reconnectionDelay: 1000, // Time delay between reconnection attempts
+    });
 
 socket.on('joinError', (message) => {
     alert(message);
+});
+
+socket.on('connect_error', (err) => {
+    console.error('Connection Error:', err);
+});
+
+socket.on('reconnect_attempt', () => {
+    console.info('Reconnecting...');
+});
+
+socket.on('connect', () => {
+    console.info('Connected to WebSocket server');
+});
+
+socket.on('disconnect', () => {
+    console.warn('Disconnected from WebSocket server');
 });
