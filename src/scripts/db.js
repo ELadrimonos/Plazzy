e34cf74c9e460ed2601634c95d56f692b563bf26
Adrián Primo <aprimo.developer@gmail.com>
2025-05-19 10:42:45 +0200

    Update db.js a .env

--- src/scripts/db.js
+++ src/scripts/db.js
@@ -2,11 +2,15 @@ const mysql = require('mysql');
 require('dotenv').config(); // Cargar las variables de entorno desde .env
 
 const db = mysql.createConnection({
-    host: process.env.AZURE_MYSQL_HOST,
-    user: process.env.AZURE_MYSQL_USER,
-    password: process.env.AZURE_MYSQL_PASSWORD,
-    database: 'plazzy',
-    port: process.env.AZURE_MYSQL_PORT
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT || 3306,
+    ssl: {
+        // Necesario para conexiones SSL a DigitalOcean MySQL
+        rejectUnauthorized: true
+    }
 });
 
 db.connect((err) => {
